feb44c4f36ed3afacb529b636acb29c2c1ac0380
coofzilla <jeric614@yahoo.com>
2022-04-07 19:57:46 +0900

    removed headers

--- src/api/kidsloop.ts
+++ src/api/kidsloop.ts
@@ -2,7 +2,4 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: " https://my-json-server.typicode.com/kidsloop-test/accounts",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
