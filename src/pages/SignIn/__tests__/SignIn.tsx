6c7825b520e1ba86259379a3892e00275842b7e3
coofzilla <jeric614@yahoo.com>
2022-04-07 16:06:18 +0900

    find 2 signins

--- src/pages/SignIn/__tests__/SignIn.tsx
+++ src/pages/SignIn/__tests__/SignIn.tsx
@@ -1,9 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { getAllByText, render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
 import SignIn from "pages/SignIn/SignIn";
 
 test("should show sign in form", () => {
-    render(<SignIn switchTheme={() => {}} theme="" />, { wrapper: MemoryRouter });
-    const input = screen.getByText("Email or Phone");
+  render(<SignIn switchTheme={() => {}} theme="" />, { wrapper: MemoryRouter });
+
+  const signin = screen.getAllByText("Sign In");
+  expect(signin).toHaveLength(2);
 });
